6ddbe542f4b010e195d95d5a7d1ceffc5a20ff1b
intrxx <michaloginski15@gmail.com>
2023-12-18 14:14:11 +0100

    Pickup class

---
+++ Source/Blaster/Private/Pickups/BPickup.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickups/BPickup.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/SphereComponent.h"
+
+ABPickup::ABPickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+
+	OverlapSphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("OverlapSphere"));
+	OverlapSphereComp->SetupAttachment(RootComponent);
+	OverlapSphereComp->SetSphereRadius(150.f);
+	OverlapSphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapSphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapSphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	PickupMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMeshComp"));
+	PickupMeshComp->SetupAttachment(OverlapSphereComp);
+	PickupMeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void ABPickup::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(HasAuthority())
+	{
+		OverlapSphereComp->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnSphereOverlap);
+	}
+}
+
+void ABPickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+}
+
+void ABPickup::Destroyed()
+{
+	Super::Destroyed();
+
+	if(PickupSound)
+	{
+		UGameplayStatics::PlaySound2D(this, PickupSound);
+	}
+}
+
+void ABPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                               UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+
+
---
+++ Source/Blaster/Public/Pickups/BPickup.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BPickup.generated.h"
+
+class USoundCue;
+class USphereComponent;
+
+UCLASS()
+class BLASTER_API ABPickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ABPickup();
+	
+	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
+	
+public:
+	
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "Blaster|Pickup")
+	TObjectPtr<USphereComponent> OverlapSphereComp;
+
+	UPROPERTY(EditAnywhere, Category = "Blaster|Pickup|Sound")
+	TObjectPtr<USoundCue> PickupSound;
+
+	UPROPERTY(EditAnywhere, Category = "Blaster|Pickup")
+	TObjectPtr<UStaticMeshComponent> PickupMeshComp;
+	
+};
